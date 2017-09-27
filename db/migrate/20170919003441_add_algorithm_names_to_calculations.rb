5df4c3f653f5be663fb5ed8bb9c7ba34846e5a97
Dante Soares <dante.m.soares@rice.edu>
2017-09-27 10:24:41 -0500

    Fixed migration errors

--- db/migrate/20170919003441_add_algorithm_names_to_calculations.rb
+++ db/migrate/20170919003441_add_algorithm_names_to_calculations.rb
@@ -13,7 +13,7 @@ class AddAlgorithmNamesToCalculations < ActiveRecord::Migration[5.0]
       dir.up do
         EcosystemMatrixUpdate
           .joins(:algorithm_ecosystem_matrix_updates)
-          .select(:id, '"algorithm_ecosystem_matrix_updates"."algorithm_name"')
+          .select(:id, :algorithm_names, '"algorithm_ecosystem_matrix_updates"."algorithm_name"')
           .find_each do |ecosystem_matrix_update|
           ecosystem_matrix_update.algorithm_names << ecosystem_matrix_update.algorithm_name
           ecosystem_matrix_update.save(validate: false)
@@ -21,7 +21,7 @@ class AddAlgorithmNamesToCalculations < ActiveRecord::Migration[5.0]
 
         ExerciseCalculation
           .joins(:algorithm_exercise_calculations)
-          .select(:id, '"algorithm_exercise_calculations"."algorithm_name"')
+          .select(:id, :algorithm_names, '"algorithm_exercise_calculations"."algorithm_name"')
           .find_each do |exercise_calculation|
           exercise_calculation.algorithm_names << exercise_calculation.algorithm_name
           exercise_calculation.save(validate: false)
@@ -29,7 +29,7 @@ class AddAlgorithmNamesToCalculations < ActiveRecord::Migration[5.0]
 
         StudentClueCalculation
           .joins(:algorithm_student_clue_calculations)
-          .select(:id, '"algorithm_student_clue_calculations"."algorithm_name"')
+          .select(:id, :algorithm_names, '"algorithm_student_clue_calculations"."algorithm_name"')
           .find_each do |student_clue_calculation|
           student_clue_calculation.algorithm_names << student_clue_calculation.algorithm_name
           student_clue_calculation.save(validate: false)
@@ -37,7 +37,7 @@ class AddAlgorithmNamesToCalculations < ActiveRecord::Migration[5.0]
 
         TeacherClueCalculation
           .joins(:algorithm_teacher_clue_calculations)
-          .select(:id, '"algorithm_teacher_clue_calculations"."algorithm_name"')
+          .select(:id, :algorithm_names, '"algorithm_teacher_clue_calculations"."algorithm_name"')
           .find_each do |teacher_clue_calculation|
           teacher_clue_calculation.algorithm_names << teacher_clue_calculation.algorithm_name
           teacher_clue_calculation.save(validate: false)
