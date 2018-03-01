f8d16c664897ab1e7dd51233759822cbec93a6a0
Dante Soares <dante.m.soares@rice.edu>
2018-03-01 13:54:29 -0600

    Filter scout requests

--- app/controllers/ecosystem_matrices_controller.rb
+++ app/controllers/ecosystem_matrices_controller.rb
@@ -1,6 +1,8 @@
 class EcosystemMatricesController < JsonApiController
 
   def fetch_ecosystem_matrix_updates
+    ScoutApm::RequestManager.lookup.ignore_request!
+
     respond_with_json_apis_and_service(
       input_schema: _fetch_ecosystem_matrix_updates_request_payload_schema,
       output_schema: _fetch_ecosystem_matrix_updates_response_payload_schema,
