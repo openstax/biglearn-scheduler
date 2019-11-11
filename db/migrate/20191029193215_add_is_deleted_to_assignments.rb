1e59723c87cfdf2235f17ae036d4b73446e99ce2
Dante Soares <dante.m.soares@rice.edu>
2019-11-11 13:21:27 -0600

    Convert IsDeleted migration into a background migration

--- db/migrate/20191029193215_add_is_deleted_to_assignments.rb
+++ db/migrate/20191029193215_add_is_deleted_to_assignments.rb
@@ -4,11 +4,20 @@ class AddIsDeletedToAssignments < ActiveRecord::Migration[5.2]
 
     reversible do |dir|
       dir.up do
-        Services::FetchCourseEvents::Service.process(
-          event_types: [ :create_update_assignment ], restart: true
-        )
-
         change_column_null :assignments, :is_deleted, false, false
+
+        # A simple background migration with the drawback that it will not retry errors
+        background_migration = -> do
+          # Become a daemon so the calling process can exit successfully
+          Process.daemon
+
+          Services::FetchCourseEvents::Service.process(
+            event_types: [ :create_update_assignment ], restart: true
+          )
+        end
+
+        # This code will run after all foreground migrations have finished
+        at_exit { background_migration.call }
       end
     end
   end
